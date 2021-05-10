deef4e21c37324e0e4196747654b77fac5cce548
xixi <yu.yi@ctrip.com>
2021-05-10 19:26:01 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import glslPlugin from './glsl-plugin';
+const { resolve } = require('path')
 export default {
     plugins: [
         glslPlugin()
@@ -8,5 +9,14 @@ export default {
             find: 'common',
             replacement: '/common'   //不用slash而用./的话地址就会resolve成"@fs/common"
         }]
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                drawTriangle: resolve(__dirname, 'draw-triangle/index.html'),
+                camera: resolve(__dirname, 'camera/index.html')
+            }
+        }
     }
 }
